9fab849b5640caf8ce7ef34e96154f393f545f28
Yu2002s <jiangdongyu54@gmail.com>
2024-11-17 14:56:35 +0800

    修复问题

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,6 +1,5 @@
 import { defineClientConfig } from 'vuepress/client'
 import RepoCard from 'vuepress-theme-plume/features/RepoCard.vue'
-
 // import CustomComponent from './theme/components/Custom.vue'
 // import Swiper from 'vuepress-theme-plume/features/Swiper.vue'
 
